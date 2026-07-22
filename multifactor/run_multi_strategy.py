f5506e65f475cb71c68ab34e414f34fb3ac26146
Dave <dave@example.com>
2026-07-22 00:05:43 -0400

    feat: optimize sub-strategy n_stocks (Growth 25, Momentum 10, Sector 15, Value 20, Quality 20); improve combined Sharpe to 1.295

--- multifactor/run_multi_strategy.py
+++ multifactor/run_multi_strategy.py
@@ -55,11 +55,11 @@ def build_portfolio(args) -> StrategyPortfolio:
     else:
         strategies_config = [
             {'name': 'multifactor', 'class': 'MultiFactorStrategy', 'weight': 0.00, 'params': {'weight_method': 'risk_parity'}},
-            {'name': 'growth', 'class': 'GrowthStrategy', 'weight': 0.35, 'params': {'weight_method': 'momentum_weighted'}},
-            {'name': 'momentum', 'class': 'MomentumStrategy', 'weight': 0.10, 'params': {'weight_method': 'momentum_weighted'}},
-            {'name': 'sector_rotation', 'class': 'SectorRotationStrategy', 'weight': 0.30, 'params': {'top_sectors': 4, 'sector_lookback': 80, 'sector_weight': 0.4}},
-            {'name': 'value', 'class': 'ValueStrategy', 'weight': 0.15, 'params': {'weight_method': 'equal'}},
-            {'name': 'quality', 'class': 'QualityStrategy', 'weight': 0.10, 'params': {'weight_method': 'risk_parity'}},
+            {'name': 'growth', 'class': 'GrowthStrategy', 'weight': 0.35, 'params': {'weight_method': 'momentum_weighted', 'n_stocks': 25}},
+            {'name': 'momentum', 'class': 'MomentumStrategy', 'weight': 0.10, 'params': {'weight_method': 'momentum_weighted', 'n_stocks': 10}},
+            {'name': 'sector_rotation', 'class': 'SectorRotationStrategy', 'weight': 0.30, 'params': {'top_sectors': 4, 'sector_lookback': 80, 'sector_weight': 0.4, 'n_stocks': 15}},
+            {'name': 'value', 'class': 'ValueStrategy', 'weight': 0.15, 'params': {'weight_method': 'equal', 'n_stocks': 20}},
+            {'name': 'quality', 'class': 'QualityStrategy', 'weight': 0.10, 'params': {'weight_method': 'risk_parity', 'n_stocks': 20}},
         ]
 
     strategies = []
